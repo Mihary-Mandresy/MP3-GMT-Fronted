131c237fe58dfcec11c3f43ea1a4d8998fb54b71
Mihary-Mandresy <miharymandresy12@gmail.com>
2026-06-18 11:15:50 +0300

    feat: axios global with authorization

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,24 @@
+import axios from "axios"
+
 export const HOST = 'localhost'
 export const PORT = 8000
 
 export const URL = `http://${HOST}:${PORT}/api`
+
+
+const api = axios.create({
+    baseURL: URL,
+    headers: {
+        "Content-Type": "application/json"
+    }
+})
+
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
+    if (token) config.headers.Authorization = `Bearer ${token}`
+    return config
+})
+
+export {
+    api
+};
