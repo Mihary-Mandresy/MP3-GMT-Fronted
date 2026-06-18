86a7431253af93652f71f5bdb3a924c32457422a
Mihary-Mandresy <miharymandresy12@gmail.com>
2026-06-18 11:18:44 +0300

    test: logout

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -1,5 +1,15 @@
+import { useNavigate } from "react-router-dom";
+import { logout } from "../../services/AuthService"
+
 export function Layout() {
+    const navigate = useNavigate();
     return <>
         Bonjour
+        <button onClick={async () => {
+            const rep = await logout();
+            if (rep.status == 200) {
+                navigate("/login")
+            }
+        }}>Logout</button>
     </>
 }
