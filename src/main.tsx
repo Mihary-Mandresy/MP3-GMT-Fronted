4d626dc57bf4d91b6b4bc40f21a31122d43fa934
Mihary-Mandresy <miharymandresy12@gmail.com>
2026-06-18 21:36:09 +0300

    feat: index.scss

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './tailwind.css'
+import "./assets/styles/index.scss"
 import App from './App.tsx'
 import { AuthProvider } from './contexts/AuthContext.tsx'
 
