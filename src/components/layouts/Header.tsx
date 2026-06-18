a4f3343968c9816e1501e52ee3c59c672fb99249
Mihary-Mandresy <miharymandresy12@gmail.com>
2026-06-18 21:37:25 +0300

    feat: menu collapse

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,25 +1,39 @@
 import { useNavigate } from "react-router-dom";
 import { logout } from "../../services/AuthService";
 import { useAuthContext } from "../../contexts/AuthContext";
+import { Menu } from "lucide-react"
+import { useCollapseContext } from "../../contexts/CollapseContext";
 
 export default function Header() {
 
     const navigate = useNavigate();
-    const { auth  } = useAuthContext()!;
+    const { auth } = useAuthContext()!;
 
     const handleLogout = async () => {
         const rep = await logout();
         if (rep.status == 200) {
             navigate("/login")
         }
-    }   
+    }
+
+    const { toggleCollapse, isCollapsed } = useCollapseContext();
+
     return <div className="flex w-full fixed top-0 h-14 bg-red-500 z-10">
-        <div className="h-full w-70 flex bg-blue-100 items-center justify-center">
+        <div className="h-full w-70 flex bg-blue-100 items-center justify-center" style={{
+            width: isCollapsed ? "80px" : "280px",
+            transition: ".3s width"
+        }}>
             <h1 className="">MP3-GMT</h1>
         </div>
-        <div className="h-full flex items-center justify-center" style={{
-            width: "calc(100% - 560px)"
+        <div className="h-full flex items-center justify-center relative" style={{
+            width: isCollapsed ? "calc(100% - 360px)" : "calc(100% - 560px)",
+            transition: ".3s width"
         }}>
+            <button id="collapse" className="absolute left-2 text-md cursor-pointer" onClick={() => {
+                toggleCollapse();
+            }}>
+                <Menu size={23} />
+            </button>
             <h1 className="text-3xl">Ceci est une titre</h1>
         </div>
         <div className="h-full w-70 bg-blue-100 flex items-center justify-evenly">
--- src/components/layouts/menu/MenuItem.tsx
+++ src/components/layouts/menu/MenuItem.tsx
@@ -3,17 +3,40 @@ import type { MenuItemProps } from "./Menu"
 import { AnimatePresence, motion } from "framer-motion";
 import { NavLink } from "react-router-dom";
 import { useRef, useState } from "react";
-import { BsChevronRight } from "react-icons/bs";
+import { BsChevronRight, BsMenuApp } from "react-icons/bs";
 
-export const MenuItem: React.FC<MenuItemProps> = ({ path, title, icon, children }) => {
+export const MenuItem: React.FC<MenuItemProps & {
+    collapse: boolean
+}> = ({ path, title, Icon, children, collapse }) => {
+    return collapse ?
+        <MenuItemCollapse
+            path={path}
+            title={title}
+            Icon={Icon}
+            children={children}
+        /> :
+        <MenuItemFull
+            path={path}
+            title={title}
+            Icon={Icon}
+            children={children}
+        />;
+
+}
+
+const MenuItemFull: React.FC<MenuItemProps> = ({ path, title, Icon, children }) => {
     const [open, setOpen] = useState<boolean>(false);
     const hasChild = useRef<boolean>(Boolean(children));
 
-    return <motion.div className="pl-8 bg-green-400">
+    return <motion.div className="pl-6 bg-green-400">
         <div className="flex items-center py-2 relative" onClick={() => {
             setOpen(!open);
         }}>
-            {icon && icon}
+            <span className="mr-4" style={{
+                fontSize: "17px"
+            }}>
+                {Icon ? <Icon /> : <BsMenuApp />}
+            </span>
             <NavLink to={path}>{title}</NavLink>
             {hasChild.current && <motion.span
                 className="absolute right-6"
@@ -33,8 +56,32 @@ export const MenuItem: React.FC<MenuItemProps> = ({ path, title, icon, children 
                     height: 0
                 }}
             >
-                {children?.map(menu => <MenuItem {...menu} />)}
+                {children?.map(menu => <MenuItem {...menu} collapse={false} />)}
             </motion.div>}
         </AnimatePresence>}
+    </motion.div>
+}
+
+function MenuItemCollapse({ path, Icon, children }: MenuItemProps) {
+    const hasChild = useRef<boolean>(Boolean(children));
+    return <motion.div className="p-4 mi-collapse relative">
+        <NavLink className="flex items-center justify-center" to={path}>
+            <span style={{
+                fontSize: "30px"
+            }}>
+                {Icon ? <Icon /> : <BsMenuApp />}
+            </span>
+        </NavLink>
+
+        {hasChild && <div className="items">
+            {children?.map(menu => <MenuItem
+                path={menu.path}
+                title={menu.title}
+                Icon={menu.Icon}
+                children={menu.children}
+                collapse={false}
+            />)}
+        </div>}
+
     </motion.div>
 }
